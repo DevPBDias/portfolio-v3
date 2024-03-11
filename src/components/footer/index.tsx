197ca247659d5a9e9ee0c45a34768b52a292df30
DevPBDias <devpbdias@gmail.com>
2024-03-11 16:23:56 -0300

    feat: creatd footer and styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { FooterContainer, IconsContainer, TextContainer } from './styles'
+import { Link } from 'react-router-dom'
+import githubIcon from '../../assets/GitHub.png'
+import linkedInIcon from '../../assets/LinkedIn.png'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <FooterContainer>
+      <TextContainer>
+        <span>&copy; UX Designer & Web Developer PBDias</span>
+      </TextContainer>
+      <IconsContainer>
+        <Link className='icons' to="https://github.com/DevPBDias/DevPBDias">
+          <img src={githubIcon} alt="Github icon" />
+        </Link>
+        <Link className='icons' to="https://github.com/DevPBDias/DevPBDias">
+          <img src={linkedInIcon} alt="Linkedin icon" />
+        </Link>
+      </IconsContainer>
+    </FooterContainer>
   )
 }
 
---
+++ src/components/footer/styles.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+`;
+
+export const TextContainer = styled.div`
+    width: 85%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+
+    span {
+    }
+`;
+
+export const IconsContainer = styled.nav`
+    width: 10%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    justify-items: center;
+
+    .icons {
+        width: 2.5em;
+        height: 2.5em;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+`;
