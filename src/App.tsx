a78da5e86f3cf93e59993d2a16db49e0167299c4
DevPBDias <devpbdias@gmail.com>
2024-03-11 15:42:24 -0300

    feat: created outlet component and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Layout from './components/layout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   )
 }
