dbb135f5677148154d533b559ec23d579e3066f0
DevPBDias <devpbdias@gmail.com>
2024-03-17 09:49:30 -0300

    fix: removed uncecessary btn

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,17 +1,11 @@
 import ProjectCard from "../../components/card/ProjectCard"
 import { projectsData } from "../../data/projectsData"
-import { BtnsContainer, PortContainer, ProjectsContainer } from "./styles"
+import { PortContainer, ProjectsContainer } from "./styles"
 
 function Portfolio() {
   return (
     <PortContainer>
       <h4>Portfolio</h4>
-      <BtnsContainer>
-        <button type="button">All projects</button>
-        <button type="button">Trybe projects</button>
-        <button type="button">Freelances</button>
-        <button type="button">Personal projects</button>
-      </BtnsContainer>
       <ProjectsContainer>
         {
           projectsData.map((project, index) => (
